f16734792257309b1abc5159d74a7c2db00779a9
mukulEDITH <125971038+mukulEDITH@users.noreply.github.com>
2025-01-26 18:30:56 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import CodeCompiler from './CodeCompiler.js';
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div>
+    <h1>Code compiler</h1>
+    <CodeCompiler/>
+    </div>
+  );
+};
+
+export default App;
